f8bca05ae0c4b23826875f30acb610c73537e384
shun471 <84883051+shun471@users.noreply.github.com>
2025-07-21 11:22:44 +0900

    raspyAPIconfig

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,8 +4,10 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000',
-    NEXT_PUBLIC_WS_URL: process.env.NEXT_PUBLIC_WS_URL || 'ws://localhost:3000',
+    // NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000',
+    // NEXT_PUBLIC_WS_URL: process.env.NEXT_PUBLIC_WS_URL || 'ws://localhost:3000',
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || '192.168.10.113:3000',
+    NEXT_PUBLIC_WS_URL: process.env.NEXT_PUBLIC_WS_URL || '192.168.10.113:3000',
   },
 };
 
