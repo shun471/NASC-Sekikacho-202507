2fe7643dc0b0c963b0acfbbc11606f0b68304b77
shun471 <84883051+shun471@users.noreply.github.com>
2025-07-23 07:15:22 +0900

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -7,7 +7,7 @@ const nextConfig: NextConfig = {
     // NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000',
     // NEXT_PUBLIC_WS_URL: process.env.NEXT_PUBLIC_WS_URL || 'ws://localhost:3000',
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://192.168.4.1:3000',
-    NEXT_PUBLIC_WS_URL: process.env.NEXT_PUBLIC_WS_URL || 'ws://192.168.1.4:3000',
+    NEXT_PUBLIC_WS_URL: process.env.NEXT_PUBLIC_WS_URL || 'ws://192.168.4.1:3000',
   },
 };
 
